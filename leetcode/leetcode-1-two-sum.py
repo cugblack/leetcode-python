c86e9086e68370ee69537eca9bab37be048c1465
edwinhei <edwinhei@tencent.com>
2022-03-07 23:09:05 +0800

    两数之和:给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。

--- leetcode/leetcode-1-two-sum.py
+++ leetcode/leetcode-1-two-sum.py
@@ -1,33 +1,18 @@
 # 1. 两数之和
 # https://leetcode-cn.com/problems/two-sum/
 
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+class Solution:
+    def twoSum(self, nums, target: int):
+        if not nums:
+            return -1
+        for i in range(len(nums)):
+            num = target - nums[i]
+            if num in nums and i != nums.index(num):
+                return [i, nums.index(num)]
 
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
+nums = [3, 3]
+target = 6
 
-        divmod() 函数把除数和余数运算结果结合起来，返回一个包含商和余数的元组
-        """
-        prenode = ListNode(0)
-        lastnode = prenode
-        val = 0
-        while val or l1 or l2:
-            val, cur = divmod(val + (l1.val if l1 else 0) + (l2.val if l2 else 0), 10)
-            lastnode.next = ListNode(cur)
-            lastnode = lastnode.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return prenode.next
-
-
-a = ListNode([1, 2, 3])
-b = ListNode([902])
-print(Solution().addTwoNumbers(a, b))
+l = Solution()
+print(l.twoSum(nums, target))
