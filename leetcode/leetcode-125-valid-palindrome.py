a31da2a87fdb2b1d1a29ab91048f8a223d9bff24
cugblack <18202794429@163.com>
2022-04-15 21:09:47 +0800

    调整格式

--- leetcode/leetcode-125-valid-palindrome.py
+++ leetcode/leetcode-125-valid-palindrome.py
@@ -1,15 +1,15 @@
 # 125. 验证回文串
 # https://leetcode-cn.com/problems/valid-palindrome/
 
-def isPalindrome(s: str):
+def is_palindrome(s: str):
     """
     :type s: str
     :rtype: bool
     """
     # 方法1： 双指针，头尾挨个判断，不相等则输出False，直到右指针=左指针，循环正常结束则输出True
     left = 0
-    s = "".join( a.lower() for a in s if a.isalnum())
-    right = len(s) -1
+    s = "".join(a.lower() for a in s if a.isalnum())
+    right = len(s) - 1
     while left < right:
         if s[left] != s[right]:
             return False
@@ -17,18 +17,18 @@ def isPalindrome(s: str):
         right -= 1
     return True
 
-def isPalindrome_2(s: str):
+
+def is_palindrome_2(s: str):
     """
     :type s: str
     :rtype: bool
     """
     # 方法2： 字符串反转比较
-    s = "".join( a.lower() for a in s if a.isalnum())
+    s = "".join(a.lower() for a in s if a.isalnum())
 
     return s == s[::-1]
 
 
-
 s = "A man, a plan, a canal: Panama"
-print(isPalindrome(s))
-print(isPalindrome_2(s))
+print(is_palindrome(s))
+print(is_palindrome_2(s))
