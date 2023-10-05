881914359694576fe51c5e0959307a5803661577
黑志强 <heizhiqiang@heizhiqiangdeMacBook-Pro.local>
2023-10-05 16:26:20 +0800

    leetcode/167.两数之和-ii-输入有序数组.py

---
+++ leetcode/167.两数之和-ii-输入有序数组.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=167 lang=python3
+#
+# [167] 两数之和 II - 输入有序数组
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) -1
+
+        while left < right:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
+            
+
+
+# @lc code=end
+
